13dcecefda41290cbe39e55c8720326e3b1d8e32
aleksazskola <93650537+aleksazskola@users.noreply.github.com>
2021-11-05 15:28:12 +0100

    Ажурирање pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "AAB") {
+        radio.setGroup(101)
+        radio.sendString("A")
+    } else if (receivedString == "ABB") {
+        radio.setGroup(101)
+        radio.sendString("B")
+    }
+})
 basic.forever(function () {
-	
+    radio.setGroup(100)
+    scrollbit.scrollText("DNS SERVER", 128, 50)
 })
