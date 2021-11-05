46612c188e663c73877263f7164095853bb324eb
aleksazskola <93650537+aleksazskola@users.noreply.github.com>
2021-11-05 15:40:55 +0100

    Ажурирање main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,11 @@ radio.onReceivedString(function (receivedString) {
     if (receivedString == "AAB") {
         radio.setGroup(101)
         radio.sendString("A")
+        basic.showIcon(IconNames.Yes)
     } else if (receivedString == "ABB") {
         radio.setGroup(101)
         radio.sendString("B")
+        basic.showIcon(IconNames.Yes)
     }
 })
 basic.forever(function () {
